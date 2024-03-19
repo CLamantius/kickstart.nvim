e7cb02c485ac0ac309a9143ee7f15a7f735dec16
ismael <ismae.kira@student.uibk.ac.at>
2024-03-19 12:21:21 +0100

    lazygit keymaps

--- lua/custom/plugins/lazygit.lua
+++ lua/custom/plugins/lazygit.lua
@@ -13,5 +13,12 @@ return {
     dependencies = {
       'nvim-lua/plenary.nvim',
     },
+    keys = {
+      { '<leader>gg', '<cmd>LazyGit<cr>', desc = 'Open LazyGit' },
+      { '<leader>gC', '<cmd>LazyGitConfig<cr>', desc = 'Lazy Git Config' },
+      { '<leader>gc', '<cmd>LazyGitCurrentFile<cr>', desc = 'Current File' },
+      { '<leader>gf', '<cmd>LazyGitFilter<cr>', desc = 'Filter' },
+      { '<leader>gF', '<cmd>LazyGitFilterCurrentFile<cr>', desc = 'Filter Current File' },
+    },
   },
 }
