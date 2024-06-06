30a3e2ef54a5c5d290bc8e87a0cf05d6c2d79e5d
CLamantius <ismae.kira@student.uibk.ac.at>
2024-06-06 12:37:53 +0200

    added oil.nvim

--- init.lua
+++ init.lua
@@ -136,7 +136,6 @@ require('lazy').setup({
 
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
-
   -- Here is a more advanced example where we pass configuration
   -- options to `gitsigns.nvim`. This is equivalent to the following lua:
   --    require('gitsigns').setup({ ... })
