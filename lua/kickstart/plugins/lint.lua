71d04fa7281b5d113348b8450fe45efbcc6db22b
CLamantius <is.kira.dev@gmail.com>
2025-05-01 12:06:53 +0200

    --

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -9,6 +9,13 @@ return {
         markdown = { 'markdownlint' },
       }
 
+      local mdl = require('lint').linters.markdownlint
+      mdl.args = {
+        '--disable',
+        'MD013',
+        '--',
+      }
+
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
       -- instead set linters_by_ft like this:
       -- lint.linters_by_ft = lint.linters_by_ft or {}
